c23a7830aa334204b017a6d04830f60c153c6008
Esandi Liyanapathirana <esandiliyanapathirana@gmail.com>
2026-07-11 19:17:43 +0530

    Updated the InventoryOperationsManager

--- src/main/java/com/example/javafxcw/InventoryOperationsManager.java
+++ src/main/java/com/example/javafxcw/InventoryOperationsManager.java
@@ -1,156 +1,104 @@
 package com.example.javafxcw;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class InventoryOperationsManager {
 
-    private static final String file_path = "inventory_cleand.txt";
+    private static final String file_path = "inventory_cleaned.txt";
     private static final String tempory_file_path = "tempory_file.txt";
 
-    public static void addItem() {
-        Scanner scanner = new Scanner(System.in);
-        boolean exists = true;
-        String code = "";
-
-        while(exists) {
-            System.out.println("Enter the item code:");
-            code = scanner.nextLine().trim();
-            exists = false;
-
-            try (BufferedReader reader = new BufferedReader(new FileReader(file_path))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] data = line.split("\\|");
-                    if (data.length > 0 && data[0].trim().equals(code)){
-                        System.out.println("Duplicate Found!");
-                        exists = true;
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("Error!: Could not read the file!");
-            }
-            System.out.println();
-        }
-
-        String name = promptUser(scanner, "Enter the item Name; ");
-        String brand = promptUser(scanner, "Enter the item Brand; ");
-        String price = promptUser(scanner, "Enter the item Price; ");
-        String quantity = promptUser(scanner, "Enter the item Quantity; ");
-        String category = promptUser(scanner, "Enter the item Category; ");
-        String date = promptUser(scanner, "Enter the Date (YYYY-MM_DD); ");
-        String image = promptUser(scanner, "Enter the item image file Name; ");
-        String threshold = promptUser(scanner, "Enter the item Threshold; ");
-
-        String record = String.join("|", code, name, brand, price, quantity, category, date, image, threshold);
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file_path, true))) {
-            writer.write(record);
-            writer.newLine();
-            System.out.println("Successfully added item:" + name);
-        } catch (IOException e) {
-            System.err.println("Error! Can't write to file" + e.getMessage());
-        }
-    }
-
-    public static void deleteItems() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the code of the item that you want to delete: ");
-        String targetCode = scanner.nextLine().trim();
-
-        File originalFile = new File(file_path);
-        File temporyFile = new File(tempory_file_path);
-        boolean exists = false;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(originalFile));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(temporyFile))) {
-
+    private static boolean isDuplicate(String code) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(file_path))) {
             String line;
-            while((line = reader.readLine()) != null) {
-                String [] data = line.split("\\|");
-
-                if (data.length > 0 && data[0].trim().equals(targetCode)) {
-                    exists = true;
-                    continue;
+            while((line = reader.readLine()) != null ) {
+                if(line.split("\\|")[0].trim().equals(code.trim())) {
+                    return true;
                 }
-
-                writer.write(line);
-                writer.newLine();
             }
-        } catch (IOException e) {
-            System.out.println("Error! Can't process the file" + e.getMessage());
-            return;
-        }
+        } catch (IOException ignored){}
+        return false;
 
-        finalizeFileReplacement(originalFile, temporyFile, exists, "Successfully deleted the item" + targetCode, "Error! Deletion Failed");
+}
+
+public static String addItem(String  code, String name, String brand, String price, String quantity, String category, String date, String image, String threshold) {
+    if (isDuplicate(code)) {
+        return "Error: Item code already exists.";
     }
 
-    public static void updateItems() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the code of the item that you want to update: ");
-        String target = scanner.nextLine().trim();
+    String record = String.join("|", code, name, brand, price, quantity, category, date, image, threshold);
 
-        File originalFile = new File(file_path);
-        File temporyFile = new File(tempory_file_path);
-        boolean exists = false;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(originalFile));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(temporyFile))) {
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] data = line.split("\\|");
-
-                if (data.length > 0 && data[0].equals(target)) {
-                    exists = true;
-                    System.out.println("Item Found! Enter the new item details: ");
-
-                    String name = promptUser(scanner,"Enter the new item Name: ");
-                    String brand = promptUser(scanner,"Enter the new item Brand: ");
-                    String price = promptUser(scanner,"Enter the new item Price: ");
-                    String quantity = promptUser(scanner,"Enter the new item Quantity: ");
-                    String category = promptUser(scanner,"Enter the new item Category: ");
-                    String date = promptUser(scanner,"Enter the new Date(YYYY-MM-DD): ");
-                    String image = promptUser(scanner,"Enter the new image file name: ");
-                    String threshold = promptUser(scanner,"Enter the new Threshold: ");
-
-                    line =  String.join("|", target, name, brand, price, quantity, category, date, image, threshold);
-                }
-
-                writer.write(line);
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error! Cant process the file " + e.getMessage());
-            return;
-        }
-
-        finalizeFileReplacement(originalFile, temporyFile, exists, "Successfully udated the item " + target, "Error! Update Failed ");
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file_path, true))) {
+        writer.write(record);
+        writer.newLine();
+        return "Successfully added item: " + name;
+    } catch (IOException e) {
+        return " The changes were not save";
     }
+}
 
-    private static void finalizeFileReplacement(File originalFile, File temporyFile, boolean exists, String sucessMessage, String failureMessage) {
-        if(exists) {
-            if(originalFile.delete()) {
-                if(temporyFile.renameTo(originalFile)) {
-                    System.out.println(sucessMessage);
-                } else {
-                    System.out.println("Error! Could not update the file system");
-                }
+public static String deleteItems (String targetCode) {
+    File originalFile = new File(file_path);
+    File temporyFile = new File(tempory_file_path);
+    boolean exists = false;
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(originalFile));
+         BufferedWriter writer = new BufferedWriter(new FileWriter(temporyFile))) {
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] data = line.split("\\|");
+            if (data.length > 0 && data[0].trim().equals(targetCode.trim())) {
+                exists = true;
+
             } else {
-                System.out.println(failureMessage);
+                writer.write(line);
+                writer.newLine();
             }
-        } else {
-            System.out.println("Error! The Item code does not exists");
-            temporyFile.delete();
         }
+    } catch (IOException e) {
+        return "Error! Can't process file";
     }
 
-
-    private static String promptUser(Scanner scanner, String prompt) {
-        System.out.println(prompt);
-        return scanner.nextLine().trim();
-
+    if (exists && originalFile.delete() && temporyFile.renameTo(originalFile)) {
+        return "Successfully deleted the item" + targetCode;
     }
 
+    temporyFile.delete();
+    return exists ? "Error! Could not update the file system." : "Error! The Item code does not exists";
+
+}
+
+public static String updateItems(String target, String name, String brand, String price, String quantity, String category, String date, String image, String threshold) {
+    File originalFile = new File(file_path);
+    File temporyFile = new File(tempory_file_path);
+    boolean exists = false;
+
+try (BufferedReader reader = new BufferedReader(new FileReader(originalFile));
+     BufferedWriter writer = new BufferedWriter(new FileWriter(temporyFile))) {
+
+    String line;
+    while ((line = reader.readLine()) != null) {
+        String[] data = line.split("\\|");
+        if (data.length > 0 && data[0].trim().equals(target.trim())) {
+            exists = true;
+
+            String updatedRecord = String.join("|", target, name, brand, price, quantity, category, date, image, threshold);
+            writer.write(updatedRecord);
+        } else {
+            writer.write(line);
+        }
+        writer.newLine();
+    }
+} catch (IOException e) {
+    return "Error! Can't process the file.";
+}
+
+if (exists && originalFile.delete() && temporyFile.renameTo(originalFile)) {
+    return "Successfully updated the item " + target;
+}
+
+temporyFile.delete();
+return exists ? "Error! Could not update the file system " : "Error the item code does not exists.";
+
+    }
 }
