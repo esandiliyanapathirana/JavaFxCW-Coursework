a7de0be0c8a28c9453e5c5d687234fdf98ac5476
Esandi Liyanapathirana <esandiliyanapathirana@gmail.com>
2026-07-21 12:06:14 +0530

    Updated LegacyDataCleanerController by changing the buttons in Scene Builder

--- src/main/java/com/example/javafxcw/LegacyDataCleanerController.java
+++ src/main/java/com/example/javafxcw/LegacyDataCleanerController.java
@@ -12,12 +12,19 @@ public class LegacyDataCleanerController {
     @FXML
     void CleanDealers(ActionEvent event) {
         LegacyDataCleaner.dealersClean();
+        AppData.dealerStore.load();
         ClickButtons.setText("Dealers text file cleaned.");
     }
 
     @FXML
     void CleanInventory(ActionEvent event) {
         LegacyDataCleaner.inventoryClean();
+        AppData.inventoryStore.load();
         ClickButtons.setText("Inventory text file cleaned.");
+    }
+
+    @FXML
+    void goBackToMenu(ActionEvent event) {
+        SceneNavigator.goTo(event, "MenuView.fxml");
     }
 }
